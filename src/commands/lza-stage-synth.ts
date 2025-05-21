bfeb63ce1649ee9821da42372fbc9815996c4738
Joscha <joscha.meyer@superluminar.io>
2025-05-21 15:45:26 +0200

    chore: improve docs
    after some feedback

--- src/commands/lza-stage-synth.ts
+++ src/commands/lza-stage-synth.ts
@@ -1,28 +1,22 @@
-import { Command, Option } from 'clipanion';
+import { Command } from 'clipanion';
+import { LzaStage } from './lza-stage';
 import { acceleratorConfigOutSynth } from '../luminarlz/accelerator-config-out-synth';
 import { customizationsCdkSynth } from '../luminarlz/customizations-synth';
 import { awsAcceleratorSynthStage } from '../luminarlz/lza-repository-checkout';
 
-export class LzaStageSynth extends Command {
-  static paths = [['lza', 'stage', 'synth']];
+export class LzaStageSynth extends LzaStage {
+  static paths = [[...super.namespacePath, 'synth']];
 
   static usage = Command.Usage({
-    category: 'LZA Stage',
+    category: super.category,
     description: 'Synth a LZA stage.',
     details: `
       This includes synthesizing everything as well as the LZA stage.
     `,
   });
-  stage = Option.String('--stage', {
-    description: `
-      The AWS Accelerator pipeline stage to be synthesized. Defaults to \`customizations\`.
-      You can find an overview of the different stages in the Landing Zone Accelerator on AWS pipeline documentation:
-      https://docs.aws.amazon.com/solutions/latest/landing-zone-accelerator-on-aws/awsaccelerator-pipeline.html
-    `,
-  });
 
   async execute() {
-    const stage = this.stage ?? 'customizations';
+    const stage = this.stageOrDefault;
     await customizationsCdkSynth();
     await acceleratorConfigOutSynth();
     await awsAcceleratorSynthStage({
