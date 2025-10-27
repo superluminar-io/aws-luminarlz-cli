1bc3af11909df51eb90e1cb2dc7d813d96ae8d1e
Karl Zierhofer <karl.zierhofer@superluminar.io>
2025-10-27 18:15:41 +0100

    Refactor cli test function to securely restore log spy.

--- test/test-helper/cli.ts
+++ test/test-helper/cli.ts
@@ -17,26 +17,31 @@ function createCliFor<C extends BaseContext>(...commands: CommandClass<C>[]): Cl
 
 async function runCli(cli: Cli<any>, argv: string[], temp: ReturnType<typeof useTempDir>) {
   const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-  const prevCwd = process.cwd();
-  process.chdir(temp.directory);
-  const silentStream = new Writable({
-    write(_chunk, _encoding, callback):void {
-      callback();
-    },
-  }); const code = await cli.run(argv, {
-    stdin: process.stdin,
-    stdout: silentStream,
-    stderr: silentStream,
-    cwd: () => process.cwd(),
-    env: process.env,
-  });
-  logSpy.mockRestore();
+  try {
+    const prevCwd = process.cwd();
+    process.chdir(temp.directory);
+    const silentStream = new Writable({
+      write(_chunk, _encoding, callback): void {
+        callback();
+      },
+    });
+    const code = await cli.run(argv, {
+      stdin: process.stdin,
+      stdout: silentStream,
+      stderr: silentStream,
+      cwd: () => process.cwd(),
+      env: process.env,
+    });
+    logSpy.mockRestore();
 
-  if (code !== 0) {
-    throw new CliError();
+    if (code !== 0) {
+      throw new CliError();
+    }
+    process.chdir(prevCwd);
+    return code;
+  } finally {
+    logSpy.mockRestore();
   }
-  process.chdir(prevCwd);
-  return code;
 }
 
 export { createCliFor, runCli, CliError };
