6c2651a405d8c3f36faac0a215e982930d264a12
Robert von Massow <robert.von.massow@superluminar.io>
2025-11-14 15:17:56 +0100

    add action for tests

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,12 @@
 import { typescript, github } from 'projen';
+import { JobPermission } from 'projen/lib/github/workflows-model';
 import { NpmAccess } from 'projen/lib/javascript';
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: '@superluminar-io/aws-luminarlz-cli',
-  description: 'A lean, opinionated CLI to make deployment and development with the Landing Zone Accelerator on AWS easier.',
+  description:
+    'A lean, opinionated CLI to make deployment and development with the Landing Zone Accelerator on AWS easier.',
   projenrcTs: true,
   autoDetectBin: true,
   license: 'MIT',
@@ -30,10 +32,7 @@ const project = new typescript.TypeScriptProject({
     'typescript',
     'zip-lib',
   ],
-  devDeps: [
-    'aws-sdk-client-mock',
-    'aws-sdk-client-mock-jest',
-  ],
+  devDeps: ['aws-sdk-client-mock', 'aws-sdk-client-mock-jest'],
   sampleCode: false,
   gitignore: ['/blueprints/**/package-lock.json', '/blueprints/**/yarn.lock'],
   githubOptions: {
@@ -49,7 +48,39 @@ project.tsconfigDev?.file?.addOverride('compilerOptions.types', [
   'jest',
   'aws-sdk-client-mock-jest',
 ]);
-project.tsconfigDev?.file?.addOverride('files', ['test/jest.custom-matchers.d.ts']);
+project.tsconfigDev?.file?.addOverride('files', [
+  'test/jest.custom-matchers.d.ts',
+]);
 project.tsconfigDev?.file?.addOverride('compilerOptions.allowJs', true);
+
+// GitHub Workflow for tests
+const wf = project.github!.addWorkflow('test');
+wf.on({ push: { branches: ['main'] }, pullRequest: {} });
+wf.addJobs({
+  test: {
+    permissions: { contents: JobPermission.READ },
+    runsOn: ['ubuntu-latest'],
+    steps: [
+      {
+        name: 'Checkout',
+        uses: 'actions/checkout@v5',
+        with: {
+          ref: '${{ github.event.pull_request.head.ref }}',
+          repository: '${{ github.event.pull_request.head.repo.full_name }}',
+        },
+      },
+      {
+        name: 'Setup Node',
+        uses: 'actions/setup-node@v4',
+        with: {
+          'node-version': '20', // or matrix if you want multiple versions
+          'cache': 'npm', // change to "yarn" or "pnpm" if you use those
+        },
+      },
+      { name: 'Install deps', run: 'yarn --frozen-lockfile' },
+      { name: 'Run tests', run: 'yarn projen test' },
+    ],
+  },
+});
 
 project.synth();
